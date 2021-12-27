d7833b749f8aee1d347ff4cd07eb3462207b71f9
Chris Kaltwasser <chris@chrisk.com>
2021-12-27 13:47:44 -0500

    mutable state

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
 use std::fs::File;
 use daemonize::Daemonize;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+use actix_web::{web, App, HttpServer};
+use std::sync::Mutex;
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {}", counter) // <- response with count
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -33,15 +32,21 @@ async fn main() -> std::io::Result<()> {
     match daemonize.start() {
         Ok(_) => {
             println!("Success, daemonized");
-            HttpServer::new(|| {
-                App::new()
-                    .service(hello)
-                    .service(echo)
-                    .route("/hey", web::get().to(manual_hello))
-            })
-            .bind("127.0.0.1:8080")?
-            .run()
-            .await?
+
+            let counter = web::Data::new(AppStateWithCounter {
+                counter: Mutex::new(0),
+            });
+
+            HttpServer::new(move || {
+                   // move counter into the closure
+                   App::new()
+                       // Note: using app_data instead of data
+                       .app_data(counter.clone()) // <- register the created data
+                       .route("/", web::get().to(index))
+               })
+               .bind("127.0.0.1:8080")?
+               .run()
+               .await?
         },
         Err(e) => eprintln!("Error, {}", e),
     }
